c2c9a72882a0a7249523efd74e13593bd43247f3
Yash Sharma <yash.sharma@loadshare.net>
2022-04-04 01:12:09 +0530

    commit

---
+++ src/main/java/com/blup0plup/sourcestreamkafka/SourceStreamKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.blup0plup.sourcestreamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceStreamKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceStreamKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blup0plup/sourcestreamkafka/SourceStreamKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blup0plup.sourcestreamkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceStreamKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
